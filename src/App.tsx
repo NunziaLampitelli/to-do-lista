e569b91ded2b44f4fc3d87b2af28c903eb77e017
Nunzia Lampitelli <nunzia.lampitelli@outlook.com>
2024-08-21 13:02:39 +0200

    writing the handle function for the new items in the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 
+interface Todo {
+  id: number;  
+  text: string;  
+  done: boolean;  
+}
+
 // React Function Component and I need it to get as a result a JSX element [syntax to work with js and html languages together]
 const App: React.FC = () => {
   
@@ -9,10 +15,22 @@ const App: React.FC = () => {
   >([]);  // the last part of the code line is to start with an empty arrey for the useState component ???
 
   // This is to declare the useState for a new list item when it will be written and it should follow the code in line 8 and so it should be a string to wich an id in form of number will be assigned
-  const [newTodo, setNewTodo] = useState("");  
+  const [newTodo, setNewTodo] = useState("");  // --> I should get the text of the new todo
+	const [nextId, setNextID] = useState(1); // this is to assign a number to the id
 
-	
-	
+//function for the items when they are added in the list (what must happen when using it)
+	 const handleAddTodo = () => {
+    if (newTodo.trim()) {  // I need this to verify if the field is empty 
+      //Here I add the new items 
+      setTodos([
+        ...todo,  //here I modify the list if there are already items inside
+        {
+          id: nextId,  
+          text: newTodo,  
+          done: false  // this is to start the item undone in the checkbox
+        }
+      ]);
+   
 };
 
 export default App;
